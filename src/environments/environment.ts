1b447a48b59a630c805fc71994a8063efb11db6e
weazelteles85 <claudioteles85@gmail.com>
2018-10-16 14:34:14 -0400

    start over

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  SENDGRID_API_KEY: "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"
+  production: true,
 };
 
 /*
