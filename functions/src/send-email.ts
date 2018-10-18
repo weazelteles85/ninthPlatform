2a71d037289a459710ef2838c03c34efc1d455cf
weazelteles85 <claudioteles85@gmail.com>
2018-10-18 16:16:51 -0400

    commands

--- functions/src/send-email.ts
+++ functions/src/send-email.ts
@@ -8,8 +8,8 @@ export const basicSendEmail = functions.https.onRequest((request, response) => {
 
     const name = request.query.name;
     if(!name) {
-        response.status(400).send('ERROR you must supply a name :(');
+        response.status(400).send('Last time for real');
     }
 
-    response.send('hello ${{name}}');
+    response.send('last time {name}');
 })
