9dcbf01c4373bf688dd2cc6a2809ba4796cab497
weazelteles85 <claudioteles85@gmail.com>
2018-10-23 09:52:33 -0400

    gallery Display Working

--- src/app/rendering/rendering.page.ts
+++ src/app/rendering/rendering.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+//import { ImageModalComponent } from './image-modal/image-modal.component';
 
 @Component({
   selector: 'app-rendering',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RenderingPage implements OnInit {
 
-  constructor() { }
+  imageRefs:Array<string> = [
+    "1-1.jpg",
+    "1-1b.jpg",
+    "1-2b.jpg"
+
+  ]
+
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async openImageModal(index) {
+    console.log(index);
+    // const modal = await this.modalController.create({
+    //   component: ImageModalComponent,
+    //   componentProps: { value: 123 }
+    // });
+    // return await modal.present();
   }
 
 }
