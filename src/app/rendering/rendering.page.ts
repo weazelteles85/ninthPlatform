c4970463a0017a05559dd930c38ca6c50ca558f6
weazelteles85 <claudioteles85@gmail.com>
2018-10-24 08:32:09 -0400

    Gallery Done

--- src/app/rendering/rendering.page.ts
+++ src/app/rendering/rendering.page.ts
@@ -9,19 +9,48 @@ import { ImageModalComponent } from './image-modal/image-modal.component';
 })
 export class RenderingPage implements OnInit {
 
+  public enviromentalColor:string = 'primary';
+  public whiteSpaceColor:string = 'primary';
+
   @ViewChild('enviromentBtn') envBtn; 
   
 
   isEnviromental = false;
   isWhiteSpace = false;
 
-  imageRefs:Array<string> = [
+  whiteSpaceImg:Array<string> = [
     "1-1.jpg",
     "1-1b.jpg",
     "1-2b.jpg",
-    "1-2b.jpg",
-    "1-1.jpg",
-    "1-1.jpg",
+    "2-1b.jpg",
+    "2-2.jpg",
+    "2-2b.jpg",
+    "3-2.jpg",
+    "4-1.jpg",
+    "5-1.jpg",
+    "6-1.jpg",
+    "7-1.jpg",
+    "8-1.jpg",
+    "8-3.jpg",
+    "9-2.jpg",
+    "10-2.jpg",
+    "10-3.jpg",
+  ]
+
+  enviromentalImg:Array<string> = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
   ]
 
   constructor(public modalController: ModalController) { }
@@ -32,23 +61,35 @@ export class RenderingPage implements OnInit {
 
   async openImageModal(index) {
     console.log(index);
-    const modal = await this.modalController.create({
-      component: ImageModalComponent,
-      componentProps: { url: "/src/assets/images/Whitespace-renders/" + index },
-      cssClass: "telesModal"
-    });
-    return await modal.present();
+    if(this.isWhiteSpace) {
+      const modal = await this.modalController.create({
+        component: ImageModalComponent,
+        componentProps: { url: "/src/assets/images/Whitespace-renders/" + index },
+        cssClass: "telesModal"
+      });
+      return await modal.present();
+    }
+    else {
+      const modal = await this.modalController.create({
+        component: ImageModalComponent,
+        componentProps: { url: "/src/assets/images/3D-Renders/" + index },
+        cssClass: "telesModal"
+      });
+      return await modal.present();
+    }
   }
 
   enviromentalToggle() {
-    console.log(this.envBtn);
-    this.envBtn.setColor('red');
+    this.whiteSpaceColor = 'primary';
     this.isWhiteSpace = false;
+    this.enviromentalColor = 'tertiary';
     this.isEnviromental = true;
   }
 
   whiteSpaceToggle() {
+    this.enviromentalColor = 'primary';
     this.isEnviromental = false;
+    this.whiteSpaceColor = 'tertiary';
     this.isWhiteSpace = true;
   }
 
